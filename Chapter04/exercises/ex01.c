253ebf163753bd1232e69d20f2a42ee66af799dd
Ashley K. Smith <mail@ashleyksmith.com>
2015-12-08 23:14:17 +0000

    Completed chapter 4 exercise 1

---
+++ Chapter04/exercises/ex01.c
@@ -0,0 +1,28 @@
+/* Author: Ashley K. Smith
+ * Solution to "C Programming: A Modern Approach" chapter 4, exercise 1
+
+	(a) i = 5; j = 3;
+	    printf("%d %d", i / j, i % j);
+	
+	    1 1
+	
+	(b) i = 2; j = 3;
+	    printf("%d", (i + 10) % j);
+	
+	    0
+	
+
+	(c) i = 7; j = 8; k = 9;
+	    printf("%d", (i + 10) % k / j);
+	
+	    1
+
+	
+	(d) i = 1; j= 2; k = 3;
+	    printf("%d", (i + 5) % (j + 2) / k);
+
+	    (0.6666)
+	    0
+
+*/
+
