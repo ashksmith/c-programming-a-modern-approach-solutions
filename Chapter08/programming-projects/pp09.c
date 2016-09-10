2a47ce04de1774dc2502b6d0df2afe5032143b8d
Ashley K. Smith <mail@ashleyksmith.com>
2016-09-10 23:39:09 +0300

    Completed programming project 9

--- Chapter08/programming-projects/pp09.c
+++ Chapter08/programming-projects/pp09.c
@@ -1,11 +1,12 @@
 /* Author: Ashley K. Smith
- * Solution to "C Programming A Modern Approach" chapter 8, programming project 5
+ * Solution to "C Programming A Modern Approach" chapter 8, programming project 9
  *
  * Write a program that generates a "random walk" across a 10 x 10 array.
  * The array will contain charatcers (all '.' initially). The program must
  * randomly "walk" from each element to element, always going up, down, left or
  * right by one element. The elements visited by the program will be labeled
  * with the letter A through Z, in the order visited. 
+ *
  */
 
 #include <stdio.h>
@@ -27,61 +28,46 @@ int main(){
 	}
 	//Set A to the starting location
 	map[row][col] = letter;
+	letter++;
 
 	//while letter < 90 (Z in ascii)
 	srand((unsigned) time(&t));
-	while(letter <= 90){
-		randNum = rand() % 4;
+	while(letter < 90){
+		randNum = rand() % 4;		
 		switch(randNum){
 			case 0: //up 
-				if(((row - 1) > 0) && (map[row - 1][col]) == '.'){
+				if(((row - 1) >= 0) && (map[row - 1][col]) == '.'){
 					row = row - 1;
-					map[row][col] = letter;
-					printf("\n%c UP\n", map[row][col]);
+					map[row][col] = letter;					
 					letter++;
-					break;
-				} else {	
-					break;
+					break;	
 				}
 			case 1: //right
-				if(((col + 1) < 9) && (map[row][col + 1]) == '.'){
+				if(((col + 1) <= 9) && (map[row][col + 1]) == '.'){
 					col = col + 1;
-					map[row][col] = letter;
-					printf("\n%c RIGHT\n", map[row][col]);
+					map[row][col] = letter;					
 					letter++;
-					break;			
-				} else {
-					break;
+					break;		
 				}
 			case 2: //down
-				if(((row + 1) < 9) && (map[row + 1][col]) == '.'){
+				if(((row + 1) <= 9) && (map[row + 1][col]) == '.'){
 					row = row + 1;
 					map[row][col] = letter;
-					printf("\n%c DOWN\n ", map[row][col]);
 					letter++;
-					break;
-				} else {
-					break;
-				}
+					break;	
+				} 
 			case 3: //left
-				if(((col - 1) > 0) && (map[row][col - 1]) == '.'){
+				if(((col - 1) >= 0) && (map[row][col - 1]) == '.'){
 					col = col - 1;
-					map[row][col] = letter;
-					printf("\n%c LEFT\n", map[row][col]);
+					map[row][col] = letter;					
 					letter++;
 					break;
-				} else {
-					break;
 				}
-			default: 
-				letter++;
-				break;
 		}
-		
 	}
 
 	for(i = 0; i < 9; i++){
-		for(n = 0; n < 9; n++){
+		for(n = 0; n <= 9; n++){
 			printf(" %c ", map[i][n]);
 		}
 		printf("\n");
