c380a8dc7028058c118146fc9e68f4ac474ac521
Ashley K. Smith <mail@ashleyksmith.com>
2016-09-12 12:00:05 +0300

    Incomplete, relies on project 8 from chapter 5 which is also incomplete

---
+++ Chapter08/programming-projects/pp10.c
@@ -0,0 +1,15 @@
+/* Author: Ashley K. Smith
+ * Solution to "C Programming A Modern Approach" chapter 8, programming project 10
+ *
+ * Modify Programming Project 8 from Chapter 5 so that the departure times are stored in
+ * an array and the arrival times are stored in a section array. (The times are integers representing
+ * the number of minutes since midnight). The program will use a loop to search the array of 
+ * departure times for the one closest to the time entered by the user user.
+ */
+
+#include <stdio.h>
+int main(void){
+	
+	return 0;
+}
+
