871f58df4d4474d10e5068534c82fef90197e397
smithy <smithy@t61.lan>
2016-09-16 13:43:48 +0300

    Incomplete

---
+++ Chapter08/programming-projects/pp17.c
@@ -0,0 +1,18 @@
+/* Author: Ashley K. Smith
+ * Solution for "C Programming A Modern Approach" chapter 8, programming project 17
+ *
+ * Write a program that prints an n x n magic square. (A square arrangement of the number 1, 2... n2 in
+ * which the sums of the rows, columns, and diagonals are all the same.
+*/
+
+#include <stdio.h>
+
+int main(void){
+
+	printf("This program creates a magic square of a specified size.");
+	printf("\nThe size must be an odd number between 1 and 99.");
+	printf("\nEnter size of magic square");
+
+
+	return 0;
+}
