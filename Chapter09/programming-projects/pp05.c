e5c39c1d397edb7fd304c47c3b8ce73564f36ae3
smithy <smithy@t61.lan>
2016-10-06 18:18:18 +0300

    Incomplete, relies on ch08, pp17 which is incomplete.

---
+++ Chapter09/programming-projects/pp05.c
@@ -0,0 +1,22 @@
+/* Author: Ashley K. Smith (https://github.com/ashksmith/)
+ * Solution to "C Programming: A modern Approach" chapter 9, programming project 5
+ *
+ * Modify programming project 17 from chapter 8 so that it includes the following
+ * functions:
+ *
+ * void create_magic_square_(int n, char magic_square[n][n]);
+ * void print_magic_square(int n, char magic_square[n][n]);
+ *
+ * After obtaining the number n from the user, main will call create_magic_square,
+ * passing it an nxn array that is declared inside main. create_magic_square will
+ * fill the array with the number 1,2,..m2 as described in the original project.
+ * main will then call print_magic_square which will display the array in the format
+ * described in the original project.
+ */
+
+#include <stdio.h>
+#include <stdbool.h>
+
+int main(void){
+
+}
