cddefd6f0e9f91232b8f6e0ac759be0393b3ea10
smithy <smithy@t61.lan>
2016-10-08 15:04:40 +0300

    Going on the road, pulling to Pi to continue

---
+++ Chapter10/exercises/ex01.c
@@ -0,0 +1,31 @@
+/* Author: Ashley K. Smith (https://github.com/ashksmith/)
+ * Solution to "C Programming: A modern Approach" chapter 10, exercise 1
+ *
+ * The following program outline shows only function definitions and variable
+ * definitions.
+ *
+ * int a;
+ *
+ * void f(int b){
+ *	int c;
+ * }
+ *
+ * void g(void){
+ *	int d;
+ *	{
+ *		int e;
+ *	}
+ * }
+ *
+ * int main(void){
+ *	int f
+ * }
+ *
+ * For each of the following scopes, list all variable and parameter names visible
+ * in that scope:
+ *
+ * (a) The f function
+ * (b) The g function
+ * (c) The block in which e is declared
+ * (d) The main functioh
+ */
