6c467ff6f975c188c6922b969de8b3607cf97193
ashksmith <ashley@ashleyksmith.com>
2016-11-08 19:17:10 +0200

    Incomplete, relies on Chapter 5, pp08

---
+++ Chapter11/programming-projects/pp02.c
@@ -0,0 +1,15 @@
+/* Author: Ashley K. Smith (https://github.com/ashksmith/)
+ * Solution to "C Programming: A modern Approach" chapter 11 programming project 2
+ *
+ * Modify programming project 8 from Chapter 5 so that it includes the following
+ * function:
+ *
+ * void find_closest_flight(int destired_time, int *departure_time, int* arrival_time);
+ *
+ * This function will find the flight whose depature time is closest to desired_time
+ * (expressed in minutes since midnight). It will store the departure and arrival
+ * times of this flight (also expressed in minutes since midnight) in the variables
+ * pointed to by departure and arrival_time, respectively.
+ */
+
+//UNFINISHED. RELIES ON pp08 FROM CHAPTER 5 WHICH IS ALSO INCOMPLETE
