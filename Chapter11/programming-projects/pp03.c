467c54378d422fd15952eaf241591c68c70468e3
ashksmith <ashley@ashleyksmith.com>
2016-11-08 19:22:08 +0200

    Incomplete, relies on Chapter 6 , pp03

---
+++ Chapter11/programming-projects/pp03.c
@@ -0,0 +1,16 @@
+/* Author: Ashley K. Smith (https://github.com/ashksmith/)
+ * Solution to "C Programming: A modern Approach" chapter 11 programming project 3
+ *
+ * Modify programming project from chapter 6 so that includes the following function
+ *
+ * void reduce(int numerator, int denominator, int *reduced_numerator,
+ * 	       int *reduced denominator);
+ *
+ * numerator and denominator are the numerator and denominator of a fraction.
+ * reduced_numerator and reduced_denominator are pointers to variables in which
+ * the function will store the numerator and denominator of the fraction once it has
+ * been reduced to lowest terms.
+ *
+ */
+
+//INCOMPLETE, RELIES ON pp03, CHAPTER 6.
