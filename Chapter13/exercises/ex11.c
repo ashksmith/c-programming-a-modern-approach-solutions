58ebc1f6bd91862bdc55b1ede42f18f84bd36d52
Ashley K. Smith <ashley@ashleyksmith.com>
2017-06-19 18:08:05 +0100

    Completed exercise 11

--- Chapter13/exercises/ex11.c
+++ Chapter13/exercises/ex11.c
@@ -5,7 +5,6 @@ The Q&A section at the end of this chapter shows how the strcmp
 function might be written using array subscripting. Modify
 the function to use pointer arithmetic instead.
 */
-
 #include <stdio.h>
 
 int strcmp(char *s, char *t){
