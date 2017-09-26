1c8708458e047e0975588fd2de9f83463ba3a779
Ashley K. Smith <ashley@ashleyksmith.com>
2017-09-26 20:22:03 +0100

    Completed exercise 14

---
+++ Chapter22/exercises/ex14.c
@@ -0,0 +1,57 @@
+/* Author: Ashley K. Smith (https://github.com/ashksmith/)
+   Solution to "C Programming: A modern Approach" chapter 22, exercise 14
+
+   (a) Write your own version of the fgets function. Make it behave as much
+       like the real fgets function as possible, in particular, make sure
+       it has the proper return value.
+
+   (b) Write your own version of fputs. 
+*/
+
+#include <stdio.h>
+char *a_fgets(char *s, int size, FILE *stream);
+int b_fputs(const char *s, FILE *stream);
+
+int main(void){
+	char output[30];
+	FILE *fp = fopen("output.txt", "rw");
+	a_fgets(output, 25, stdin);
+	printf("a_fgets from stdin: %s\n", output);
+	b_fputs("Hakunamatata", fp);
+	a_fgets(output, 100, fp);
+	printf("\n%s\n", output);
+	return 0;
+}
+
+// Part A
+char *a_fgets(char *s, int size, FILE *stream){
+	int i = 0,  ch;
+	// Get characters until EOF or reach size
+	while((ch = getc(stream)) != EOF && i < size){
+		// fgets will stop after reading a new line, it's stored
+		// in the buffer too
+		if(ch == '\n'){
+			s[i++] = '\n';
+			break;
+		} else {
+			// Store character in the buffer
+			s[i++] = ch;
+		}
+	}
+	// Add terminating byte
+	s[i] = '\0';
+	// Return pointer to buffer
+	return s;
+}
+
+int b_fputs(const char *s, FILE *stream){
+	char ch;
+	// Read until end of string
+	while(*s != '\0'){
+		// put character to stream
+		if((ch = putc(*s, stream) == EOF))
+			return EOF;
+		*s++;
+	}
+	return 1;
+}
